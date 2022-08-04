123c3343f778dce259eba475d5060c4841a55870
Jocelyn Naarden <tzionone@gmail.com>
2022-08-04 07:25:58 -0400

    First Deliverable Section Done

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,6 +4,23 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
+  def get_page
+    Nokogiri::HTML(URI.open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
+  def get_courses
+    get_page.css("article.post")
+  end
+
+  def make_courses
+    courses = get_courses
+    courses.each do |course|
+      new_course = Course.new
+      new_course.title = course.css("h2").text
+      new_course.schedule = course.css("em.date").text
+      new_course.description = course.css("p").text
+    end
+  end
   
   def print_courses
     self.make_courses
